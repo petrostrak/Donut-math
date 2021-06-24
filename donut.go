4e94ee0408b6d718e119d33ba64e3d756c02815e
petros <pit.trak@gmail.com>
2021-06-24 13:16:16 +0300

    Donut math in Go

--- donut.go
+++ donut.go
@@ -21,15 +21,15 @@ func greaterThanN(N float64) int {
 }
 
 // k % TERM_WIDTH ? ((char*) b)[k] : '\n'
-func rewrite(k, term_width int, b [][]byte) []byte {
-	if k%term_width == 0 {
+func rewrite(k, term_width int, b [TERM_HEIGHT][TERM_WIDTH]rune) [TERM_WIDTH]rune {
+	if math.Mod(float64(k), float64(term_width)) == 0 {
 		return b[k]
 	}
 
 	return b['\n']
 }
 
-func populateByte(b [][]byte) {
+func populateByte(b [TERM_HEIGHT][TERM_WIDTH]rune) {
 	for i := 0; i < TERM_HEIGHT; i++ {
 		for j := 0; j < TERM_WIDTH; j++ {
 			b[i][j] = ' '
@@ -37,7 +37,7 @@ func populateByte(b [][]byte) {
 	}
 }
 
-func populateFloat(z [][]float64) {
+func populateFloat(z [TERM_HEIGHT][TERM_WIDTH]float64) {
 	for i := 0; i < TERM_HEIGHT; i++ {
 		for j := 0; j < TERM_WIDTH; j++ {
 			z[i][j] = 0
@@ -50,8 +50,8 @@ func main() {
 	var B float64 = 0
 	var i, j float64
 	var k int
-	z := make([][]float64, TERM_WIDTH*TERM_HEIGHT)
-	b := make([][]byte, TERM_WIDTH*TERM_HEIGHT)
+	z := [TERM_HEIGHT][TERM_WIDTH]float64{}
+	b := [TERM_HEIGHT][TERM_WIDTH]rune{}
 
 	fmt.Printf("\x1b[2J")
 	for {
@@ -76,13 +76,14 @@ func main() {
 				N := 8 * ((f*e-c*d*g)*m - c*d*e - f*g - l*d*n)
 				if TERM_HEIGHT > y && y > 0 && x > 0 && TERM_WIDTH > x && D > z[y][x] {
 					z[y][x] = D
-					b[y][x] = ".,-~:;=!*#$@"[greaterThanN(N)]
+					// ".,-~:;=!*#$@"[ N > 0 ? N : 0 ]
+					b[y][x] = [12]rune{'.', ',', '-', '~', ':', ';', '=', '!', '*', '#', '$', '@'}[greaterThanN(N)]
 				}
 			}
 		}
 		fmt.Printf("\x1b[H")
 		for k = 0; k < 1761; k++ {
-			fmt.Fprint(os.Stdout, rewrite(k, TERM_WIDTH, b))
+			fmt.Fprintln(os.Stdout, rewrite(k, TERM_WIDTH, b))
 			A += 0.00004
 			B += 0.00002
 		}
